259f1c6df33d7803c8034501eff6fda90182cee8
Jeffrey Weng <jweng2014@hotmail.com>
2018-01-31 21:34:53 -0500

    Update VizIoT.js

--- src/containers/VizIoT.js
+++ src/containers/VizIoT.js
@@ -17,23 +17,19 @@ import { selectAggregateSample } from '../selectors/aggregateSampleSelector';
 class VizIoT extends React.Component {
   state = {
     showDeviceList: true,
-    timer: null,
-    counter: 4,
   }
 
-  tick = () => {
-    this.setState({
-      counter: this.state.counter + 5
-    })
+  componentWillMount() {
+    fetchActionGetTestLogEvents()
   }
 
-  componentDidMount () {
-    // let timer = setInterval(this.tick, 1000);
-    // this.setState({timer});
-  }
-
-  componentWillMount () {
-    // clearInterval(this.state.timer);
+  createDummyData() {
+    const toReturn = [];
+    const curTime = moment();
+    for (let i = 0; i < 100; i++) {
+      toReturn.push({ time_stamp: curTime.clone().add(i, 'seconds'), tally: 0 })
+    }
+    return toReturn;
   }
 
   renderBarChartCards () {
@@ -42,9 +38,11 @@ class VizIoT extends React.Component {
       const deviceKey = `${ip}:${port}`
       console.log(`deviceKey = ${deviceKey}`)
 
-      // const thisHistData = this.props.histogramLogs[deviceKey].slice(0, this.state.counter)
-      const thisHistData = this.props.histogramLogs[deviceKey]
-      const thisTimerange = this.props.timeranges[deviceKey]
+      console.log("this.props.histogramLogs");
+      console.log(this.props.histogramLogs);
+      let possibleLog = this.props.histogramLogs[deviceKey];
+      const thisHistData = (possibleLog && possibleLog.length !== 0) ? possibleLog : this.createDummyData()
+      const thisTimerange = {start: moment(), end: moment().add(30, 'seconds')}
       console.log('thisHistData')
       console.log(thisHistData)
       console.log('thisTR')
@@ -101,6 +99,11 @@ class VizIoT extends React.Component {
             <div className="large-spacer"/>
             <div className="large-spacer"/>
             <div className="large-spacer"/>
+            <div className="large-spacer"/>
+            <div className="large-spacer"/>
+            <div className="large-spacer"/>
+
+
 
           </div>
         </div>
@@ -112,13 +115,13 @@ class VizIoT extends React.Component {
 const mapStateToProps = (state) => {
   return {
     devices: selectAllDevices(state),
-    histogramLogs: {"192.168.10.115:39490": selectAggregateSample(state)},
+    histogramLogs: selectAggregateSample(state),
     timeranges: selectAllTimeranges(state),
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
-  fetchActionGetTestLogEvents()
+
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(VizIoT)
+export default connect(mapStateToProps, {fetchActionGetTestLogEvents})(VizIoT)
