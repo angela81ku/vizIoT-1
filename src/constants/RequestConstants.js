8c63ab8b98536f2d2d598dc71efbbb7b09b800af
jweng <jweng@hubspot.com>
2018-03-14 16:32:37 -0400

    Heroku URL

--- src/constants/RequestConstants.js
+++ src/constants/RequestConstants.js
@@ -1,9 +1,10 @@
 const HOSTS = {
   DEV: 'http://54.193.126.147:3000',
+  DEV_HEROKU: 'https://moniotr-smart-router-server.herokuapp.com',
   PROD: 'http://54.193.126.147:3000',
   MOCK: 'https://7f1bae45-6a9e-45db-a423-d83426b2d13f.mock.pstmn.io',
 };
-export const baseUrl = HOSTS.DEV;
+export const baseUrl = HOSTS.DEV_HEROKU;
 
 export const defaultNetwork = '42';
 
