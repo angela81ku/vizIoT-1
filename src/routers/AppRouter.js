4ce0b566dcdb2fa9a1050bba160e10d7d147acff
jweng <jweng@hubspot.com>
2018-03-14 16:22:50 -0400

    Hash router

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import VizIoT from '../containers/VizIoT';
 import NotFoundPage from '../containers/NotFoundPage';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 
 // exact prop means: exact path match
 const AppRouter = () => (
-  <BrowserRouter>
+  <HashRouter>
     <Switch>
       <Route path="/" component={VizIoT} exact={true} />
       <Route path="/404" component={NotFoundPage} />
       <Route component={NotFoundPage} />
     </Switch>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 export default AppRouter;
