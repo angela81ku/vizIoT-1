8814818b6ddfb2eeb7e9ca049df21e9dcb7c4608
beanrider <jweng2014@hotmail.com>
2018-04-09 22:58:11 -0400

    Remove console.log

--- src/utility/DataKey.js
+++ src/utility/DataKey.js
@@ -3,10 +3,5 @@
 import hash from 'object-hash';
 
 export const getDataKey = configObject => {
-  let hashValue = hash(Object.values(configObject), { unorderedArrays: true });
-  if (configObject.selectionMode === 'COMBINED') {
-    // debugger;
-    console.log(`the hash = ${hashValue}`);
-  }
-  return hashValue;
+  return hash(Object.values(configObject), { unorderedArrays: true });
 };
