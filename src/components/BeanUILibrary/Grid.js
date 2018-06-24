b6751ea281fa75089eacd38d3eef4864524f0c8c
beanrider <jweng2014@hotmail.com>
2018-06-24 13:28:52 -0400

    Improve Grid

--- src/components/BeanUILibrary/Grid.js
+++ src/components/BeanUILibrary/Grid.js
@@ -1,20 +1,42 @@
 import React from 'react';
+import classNames from 'classnames';
+import keyMirror from 'keymirror';
+import PropTypes from 'prop-types';
+
+const FlexDirection = keyMirror({
+  ROW: null,
+  COLUMN: null,
+});
+const FlexDirectionList = Object.keys(FlexDirection);
 
 class Grid extends React.Component {
   render() {
-    const { gutter, className } = this.props;
-
-    const classNames = ['flex-row', 'fade'];
-    if (gutter) {
-      classNames.push(`gutter-${gutter}`);
-    }
-
+    const { direction, gutter, className, animate } = this.props;
     return (
-      <div className={classNames.concat(className).join(' ')}>
+      <div className={classNames(className,
+        {
+          [`gutter-${gutter}`]: gutter,
+          'fade': animate,
+          'flex-row': direction === FlexDirection.ROW,
+          'flex-column': direction === FlexDirection.COLUMN,
+        }
+      )}>
         {this.props.children}
       </div>
     );
   }
 }
+
+Grid.defaultProps = {
+  animate: true,
+  direction: FlexDirection.ROW,
+  gutter: 0,
+};
+
+Grid.propTypes = {
+  animate: PropTypes.bool,
+  direction: PropTypes.oneOf(FlexDirectionList),
+  gutter: PropTypes.number,
+};
 
 export default Grid;
