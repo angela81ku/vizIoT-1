24bd8f6929fb788595c00f6804a506687bec2f35
Joseph <burns.jose@northeastern.edu>
2021-04-06 20:04:13 -0400

    fixed package.json to account for new webpack-cli update

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,46 @@
-const path = require("path");
-
-module.exports = {
-  // Entry point: direct app kicks off
-  entry: "./src/app.js",
-
-  // Output: where to output file
-  output: {
-    // Needs to be an absolute path:
-    path: path.join(__dirname, "public"),
-    filename: "bundle.js"
-  },
-
-  // loader: match pattern, and do something to matched files
-  module: {
-    rules: [{
-      loader: "babel-loader",
-      test: /\.js$/,
-      exclude: /node_modules/
-    }, {
-      test: /\.s?css$/,
-      use: [
-        'style-loader',
-        "css-loader",
-        "sass-loader",
-        "postcss-loader",
-        ""
-      ]
-    }]
-  },
-
-  externals: {
-    "react-native": true,
-  },
-
-  // converts babel source to react source in browser to debug
-  devtool: "cheap-module-eval-source-map",
-
-  devServer: {
-    contentBase: path.join(__dirname, "public"),
-    // This will enable routing on client side.
-    historyApiFallback: true
-  }
-};
-
-
+// const path = require("path");
+//
+// module.exports = {
+//   // Entry point: direct app kicks off
+//   entry: "./src/app.js",
+//
+//   // Output: where to output file
+//   output: {
+//     // Needs to be an absolute path:
+//     path: path.join(__dirname, "public"),
+//     filename: "bundle.js"
+//   },
+//
+//   // loader: match pattern, and do something to matched files
+//   module: {
+//     rules: [{
+//       loader: "babel-loader",
+//       test: /\.js$/,
+//       exclude: /node_modules/
+//     }, {
+//       test: /\.s?css$/,
+//       use: [
+//         'style-loader',
+//         "css-loader",
+//         "sass-loader",
+//         "postcss-loader",
+//         ""
+//       ]
+//     }]
+//   },
+//
+//   externals: {
+//     "react-native": true,
+//   },
+//
+//   // converts babel source to react source in browser to debug
+//   devtool: "cheap-module-eval-source-map",
+//
+//   devServer: {
+//     contentBase: path.join(__dirname, "public"),
+//     // This will enable routing on client side.
+//     historyApiFallback: true
+//   }
+// };
+//
+//
